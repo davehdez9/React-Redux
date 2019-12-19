8004e439f947517b9d7cf2a09fd22400f120ab17
David <n.dave.hernandez@gmail.com>
2019-12-18 20:33:09 -0500

    Deleting Data

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
-const Ninjas = ({ ninjas }) => {
-  // const ninjaList = ninjas.map(ninja => {
-  //   if (ninja.age > 20) {
-  //     return (
-  //       <div className="ninja" key={ninja.id}>
-  //         <div>Name: {ninja.name}</div>
-  //         <div>Age: {ninja.age}</div>
-  //         <div>Belt: {ninja.belt}</div>
-  //       </div>
-  //     );
-  //   } else {
-  //     return null;
-  //   }
-  // });
+const Ninjas = ({ ninjas, deleteNinja }) => {
+  const ninjaList = ninjas.map(ninja => {
+    if (ninja.age > 20) {
+      return (
+        <div className="ninja" key={ninja.id}>
+          <div>Name: {ninja.name}</div>
+          <div>Age: {ninja.age}</div>
+          <div>Belt: {ninja.belt}</div>
+          <button
+            onClick={() => {
+              deleteNinja(ninja.id);
+            }}
+          >
+            Delete ninja
+          </button>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  });
   // const ninjaList = ninjas.map(ninja => {
   //   return ninja.age > 20 ? (
   //     <div className="ninja" key={ninja.id}>
@@ -25,7 +32,7 @@ const Ninjas = ({ ninjas }) => {
   // });
   return (
     <div className="ninja-list">
-      {ninjas.map(ninja => {
+      {/* {ninjas.map(ninja => {
         return ninja.age > 20 ? (
           <div className="ninja" key={ninja.id}>
             <div>Name: {ninja.name}</div>
@@ -33,7 +40,8 @@ const Ninjas = ({ ninjas }) => {
             <div>Belt: {ninja.belt}</div>
           </div>
         ) : null;
-      })}
+      })} */}
+      {ninjaList}
     </div>
   );
 };
